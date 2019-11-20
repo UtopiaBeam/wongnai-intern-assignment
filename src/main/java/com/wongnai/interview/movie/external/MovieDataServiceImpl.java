ec3a3d3c58ec45f26afeb8b2ead7680e7c1031f9
UtopiaBeam <utopiabeam@gmail.com>
2019-11-20 14:13:23 +0700

    :sparkles: Change catch exception type

--- src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
+++ src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
@@ -1,17 +1,12 @@
 package com.wongnai.interview.movie.external;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestOperations;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -28,12 +23,13 @@ public class MovieDataServiceImpl implements MovieDataService {
     private ObjectMapper objectMapper;
 
     @Override
+    public MoviesResponse fetchAll() {
         // TODO:
         // Step 1 => Implement this method to download data from MOVIE_DATA_URL and fix any error you may found.
         // Please noted that you must only read data remotely and only from given source,
         // do not download and use local file or put the file anywhere else.
 
-		// Add JSON converter
+		// Add message converter
 		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
 		converter.setSupportedMediaTypes(Arrays.asList(MediaType.TEXT_PLAIN));
 		((RestTemplate) restTemplate).getMessageConverters().add(converter);
@@ -41,7 +37,7 @@ public class MovieDataServiceImpl implements MovieDataService {
 		try {
 			MoviesResponse response = restTemplate.getForObject(MOVIE_DATA_URL, MoviesResponse.class);
 			return response;
-		} catch (RestClientException e) {
+		} catch (Exception e) {
 			return null;
 		}
     }
