b673e1017aabb0cdf659684c26ec219b87e06e8d
UtopiaBeam <utopiabeam@gmail.com>
2019-11-20 15:35:36 +0700

    :sparkles: Complete TODO 2

--- src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
+++ src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
@@ -1,7 +1,12 @@
 package com.wongnai.interview.movie.search;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.wongnai.interview.movie.external.MovieData;
+import com.wongnai.interview.movie.external.MoviesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,14 +16,27 @@ import com.wongnai.interview.movie.external.MovieDataService;
 
 @Component("simpleMovieSearchService")
 public class SimpleMovieSearchService implements MovieSearchService {
-	@Autowired
-	private MovieDataService movieDataService;
+    @Autowired
+    private MovieDataService movieDataService;
 
-	@Override
-	public List<Movie> search(String queryText) {
-		//TODO: Step 2 => Implement this method by using data from MovieDataService
-		// All test in SimpleMovieSearchServiceIntegrationTest must pass.
-		// Please do not change @Component annotation on this class
-		return null;
-	}
+    @Override
+    public List<Movie> search(String queryText) {
+        MoviesResponse movies = movieDataService.fetchAll();
+
+        List<Movie> result = new ArrayList();
+        for (MovieData movie : movies) {
+            // Get lowercase words
+            List<String> words = Arrays.asList(movie.getTitle().split(" "));
+            for (int i = 0; i < words.size(); i++) {
+                words.set(i, words.get(i).toLowerCase());
+            }
+
+            if (words.contains(queryText.toLowerCase())) {
+                Movie newMovie = new Movie(movie.getTitle());
+                newMovie.setActors(movie.getCast());
+                result.add(newMovie);
+            }
+        }
+        return result;
+    }
 }
